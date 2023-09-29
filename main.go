8241b1da99cbd569c8f98917dd04857545fd5954
Smithan <tsmithanreddy@gmail.com>
2023-09-28 20:37:11 -0400

    Add areaOfCircle method

--- main.go
+++ main.go
@@ -1,17 +1,30 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+)
 
 func main() {
 	OddEven(8888)
-	OddEven(9999)   
+	OddEven(9999)
+
+	var radius, Area float64
+	log.Print("enter the radius of a CIRCLE:")
+	fmt.Scanln(&radius)
+	Area = areaOfCircle(radius)
+	log.Println("The Area of a Circle whose radius is", radius, "is", Area, "cm^2.")
 }
 
-//Check if given number is odd or even
-func OddEven (num int) {
-	if(num%2==0){
-		log.Println("Given number",num," is even number")
+// Check if given number is odd or even
+func OddEven(num int) {
+	if num%2 == 0 {
+		log.Println("Given number", num, " is even number")
 	} else {
-		log.Println("Given number",num," is odd number")
+		log.Println("Given number", num, " is odd number")
 	}
+}
+
+func areaOfCircle(radius float64) float64 {
+	return (22 / 7.0) * (radius * radius)
 }
