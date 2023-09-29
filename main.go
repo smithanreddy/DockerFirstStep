9b29451cca24a92d84113644e7087dc3dc18ec30
Manikanth Reddy <manikanthreddy669@gmail.com>
2023-09-28 20:23:47 -0400

    Add OddEven function

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "log"
 
+func main() {
+	OddEven(8888)
+	OddEven(9999)   
+}
+
+//Check if given number is odd or even
+func OddEven (num int) {
+	if(num%2==0){
+		log.Println("Given number",num," is even number")
+	} else {
+		log.Println("Given number",num," is odd number")
+	}
 }
